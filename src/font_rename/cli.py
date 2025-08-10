3814f9633c69df8d8f837722e7e78172d2992663
k2angel <90847045+k2angel@users.noreply.github.com>
2025-08-10 23:00:45 +0900

    Add: dry-run option. Add: option description.

--- src/font_rename/cli.py
+++ src/font_rename/cli.py
@@ -11,16 +11,23 @@ def main():
         nargs="*",
     )
     parser.add_argument(
+        "-d",
+        "--dry-run",
+        action="store_true",
+        help="Displays the new file names without actually renaming the files.",
+    )
+    parser.add_argument(
+        "-r",
         "--recursive",
         action="store_true",
+        help="Recursively searches subdirectories for font files.",
     )
 
     args = parser.parse_args()
     for path in args.path:
         if os.path.isdir(path):
             font_files = get_fonts(path, recursive=args.recursive)
-            if font_files:
-                for font_file in font_files:
-                    rename(font_file)
+            for font_file in font_files:
+                rename(font_file, dry_run=args.dry_run)
         else:
-            rename(path)
+            rename(path, dry_run=args.dry_run)
